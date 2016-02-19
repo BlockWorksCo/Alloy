fdedb56cf758c3a6a367fd5f48c71ab5451f31c2
Steve Tickle <Steve@ZooKoo.com>
2016-02-19 23:00:06 +0000

    -

--- Examples/Ping/Main.c
+++ Examples/Ping/Main.c
@@ -1,10 +1,14 @@
 //
+// Copyright (C) BlockWorks Consulting Ltd - All Rights Reserved.
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// Proprietary and confidential.
+// Written by Steve Tickle <Steve@BlockWorks.co>, November 2015.
 //
-//
+
 
 #include <stdint.h>
 #include <stdbool.h>
-#include "CoreServices.h"
+#include "Alloy.h"
 #include <stdarg.h>
 #include <sys/types.h>
 
